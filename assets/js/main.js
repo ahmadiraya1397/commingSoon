017e1ad96a6234f0747c92e5d674b0e15c14824d
abdollah <ahmadi.raya@gmail.com>
2022-01-08 22:16:03 +0330

    نصب فونت وزیر

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,10 +65,10 @@
   const countDownDate = function() {
     let timeleft = new Date(countdown.getAttribute('data-count')).getTime() - new Date().getTime();
 
-    let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    let days = Math.floor(timeleft / (1000 * 60 * 60 * 24))*-1;
+    let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))*-1;
+    let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60))*-1;
+    let seconds = Math.floor((timeleft % (1000 * 60)) / 1000)*-1;
 
     countdown.innerHTML = output.replace('%d', days).replace('%h', hours).replace('%m', minutes).replace('%s', seconds);
   }
